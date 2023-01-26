ffef923ce18b61c3507ff3d224f06bc2fa92eaaf
Quentin Billac <quentin.billac2@gmail.com>
2023-01-26 18:05:40 +0100

    add script fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import Demo from './components/Demo';
 
 export default function App() {
